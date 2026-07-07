bcc5d54a62a5eabe6325e29f1f13f72ffbbe4e0e
abdullah135 <abdullah@satechnology.com.au>
2026-07-07 16:04:27 +0600

    remove test folder

--- src/views/pinboards/components/test/comment/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as PinboardCommentDrawer } from './components/PinboardCommentDrawer.vue';
-export { default as PinboardCommentPanel } from './components/PinboardCommentPanel.vue';
-export { usePinboardCommentStore } from './store/pinboardCommentStore';
-export type { PinboardCommentContext, PinboardCommentRecord } from './models/PinboardComment';
